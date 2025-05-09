d65834205144753c20f507438c6650c299ba86ab
sahasra002 <sahasrarao049@gmail.com>
2025-05-09 01:25:50 -0500

    Fix Rust installation path

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Install Rust
-curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
+# Install Rust in a writable directory
+curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain stable --profile minimal --no-modify-path
+export PATH="$HOME/.cargo/bin:$PATH"
 # Proceed with the build
 pip install -r requirements.txt
