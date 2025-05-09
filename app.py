9f1c75a469901820937c61689ea960f9943d9208
sahasra002 <sahasrarao049@gmail.com>
2025-05-09 01:48:56 -0500

    Fix EOF error

--- app.py
+++ app.py
@@ -107,5 +107,4 @@ def home():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
-user.name=sahasra002
-user.email=sahasrarao049@gmail.com
+user_input = "default_value"
